78eeac170e4c65ecc5da43a45590c1f029d5025f
chendaye <chendaye666@gmail.com>
2020-03-29 16:38:14 +0800

    javaLearn

---
+++ src/Algorithm/List/Utils/ListNode.java
@@ -0,0 +1,13 @@
+package Algorithm.List.Utils;
+
+/**
+ * 链表结构
+ */
+public class ListNode {
+    public int val;
+    public ListNode next = null;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
