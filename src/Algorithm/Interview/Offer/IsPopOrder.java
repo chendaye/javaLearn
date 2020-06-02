bf124601c85d454fa8090736bbb8c4db079d5a5e
chendaye <chendaye666@gmail.com>
2020-06-03 00:34:28 +0800

    save

---
+++ src/Algorithm/Interview/Offer/IsPopOrder.java
@@ -0,0 +1,14 @@
+package Algorithm.Interview.Offer;
+
+/**
+ *
+ */
+public class IsPopOrder {
+    public boolean IsPopOrder(int [] pushA,int [] popA) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
