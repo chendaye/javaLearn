81e8a0fe78d5c68ca9502a8e0bab74fa19df5a84
chendaye <chendaye666@gmail.com>
2020-06-04 00:33:51 +0800

    BST

---
+++ src/Algorithm/Interview/Offer/FindPath.java
@@ -0,0 +1,28 @@
+package Algorithm.Interview.Offer;
+
+import java.util.ArrayList;
+
+/**
+ * 输入一颗二叉树的根节点和一个整数，打印出二叉树中结点值的和为输入整数的所有路径。
+ * 路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。
+ */
+public class FindPath {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+    }
+
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
