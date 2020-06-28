9a388322eaef4e1dcb728eebfec2b8fab5d7c901
lengo <chendaye666@gmail.com>
2020-06-28 17:01:58 +0800

    save

--- src/Algorithm/ActualCombat/List/ReverseLinkedList.java
+++ src/Algorithm/ActualCombat/List/ReverseLinkedList.java
@@ -59,4 +59,48 @@ public class ReverseLinkedList {
         }
         return pre;
     }
+
+    /**
+     * todo: 倒序输出链表
+     *    - 链表也有先序 后续遍历
+     *    - 树是链表的扩展
+     *    - 递归的实质是，把链表的的节点按顺序压栈，然后出栈
+     * @param head
+     */
+    public static void traverse(ListNode head){
+       if (head == null) return;
+       //todo: 先序
+       traverse(head.next);
+       //todo: 后序
+       System.out.println(head.val);
+    }
+
+    /**
+     * todo:通过后序遍历 判断链表是否是回文（双指针）
+     *    - 也可以新建一个逆向的链表和原来的链表比较是否相同
+     *
+     *
+     * todo: ⾸先，
+     *       - 寻找回⽂串是从中间向两端扩展
+     *       - 判断回⽂串是从两端向中间收缩。
+     *          - 对于单链表，⽆法直接倒序遍历，可以造⼀条新的反转链表，
+     *          - 可以利⽤链表的后序遍历
+     *          - 也可以⽤栈结构倒序处理单链表
+     * @param head
+     * @return
+     */
+    ListNode left; //todo: 左侧指针
+    public  boolean isPalindrome(ListNode head){
+        left = head;
+        return traverse2(head);
+    }
+    //todo: right 是一个右指针
+    public  boolean traverse2(ListNode right){
+        if (right == null) return true;
+        boolean res = traverse2(right.next);  //todo:将链表节点放入递归栈中
+        //todo: 后序
+        res = res && (left.val == right.val);
+        left = left.next;
+        return  res;
+    }
 }
