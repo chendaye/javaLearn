ee377233d87ed9daa5700fc9b19bc02e4e8d043f
lengo <chendaye666@gmail.com>
2020-06-29 17:25:30 +0800

    save

---
+++ src/Algorithm/Interview/Offer/cutRope.java
@@ -0,0 +1,35 @@
+package Algorithm.Interview.Offer;
+
+/**
+ * 题目描述
+ * 给你一根长度为n的绳子，请把绳子剪成整数长的m段（m、n都是整数，n>1并且m>1，m<=n），
+ * 每段绳子的长度记为k[1],...,k[m]。请问k[1]x...xk[m]可能的最大乘积是多少？例如，当绳子的长度是8时，
+ * 我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+ *
+ * 输入描述:
+ * 输入一个数n，意义见题面。（2 <= n <= 60）
+ *
+ * 输入 8
+ *
+ * 输出 18
+ */
+public class cutRope {
+
+    /**
+     * todo: 状态、 选择、 base case
+     *      - 知道哪些信息可以把原问题切分为重叠子问题？
+     * 长 = n  段 = m 求 = 乘积的最大值
+     *
+     *
+     *
+     * @param target
+     * @return
+     */
+    public int cutRope(int target) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
