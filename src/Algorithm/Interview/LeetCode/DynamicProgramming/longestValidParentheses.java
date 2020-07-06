a081671a18b3943a5d4bc1d4e04b0bb092d573cd
chendaye666 <chendaye666@gmail.com>
2020-07-07 00:32:07 +0800

    leetcode

---
+++ src/Algorithm/Interview/LeetCode/DynamicProgramming/longestValidParentheses.java
@@ -0,0 +1,30 @@
+package Algorithm.Interview.LeetCode.DynamicProgramming;
+
+/**
+ * 给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+ *
+ * 示例 1:
+ *
+ * 输入: "(()"
+ * 输出: 2
+ * 解释: 最长有效括号子串为 "()"
+ * 示例 2:
+ *
+ * 输入: ")()())"
+ * 输出: 4
+ * 解释: 最长有效括号子串为 "()()"
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-valid-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class longestValidParentheses {
+    /**
+     * todo: 左括号个数要大于等于右括号个数
+     * @param s
+     * @return
+     */
+    public int longestValidParentheses(String s) {
+
+    }
+}
