eddb1f0eff904bee0775540ddbcd2658b7684f72
chendaye666 <chendaye666@gmail.com>
2020-08-12 01:17:07 +0800

    save

--- src/Algorithm/Test.java
+++ src/Algorithm/Test.java
@@ -6,10 +6,6 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) throws FileNotFoundException {
-        System.setIn(new FileInputStream("E:\\learnJava\\src\\Algorithm\\Algorithm.Interview\\TX\\Q1\\test.txt"));
 
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = 0;
     }
 }
