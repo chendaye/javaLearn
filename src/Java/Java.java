ee737125bfcd294877155012e261278274bf9068
lengo <chendaye666@gmail.com>
2020-08-28 14:43:37 +0800

    save

--- src/Java/Java.java
+++ src/Java/Java.java
@@ -1,5 +1,7 @@
 package Java;
 
+import java.util.*;
+
 /**
  * 单例模式（同步）
  */
@@ -28,6 +30,46 @@ interface Car{
 class Car1 implements Car{
     public void run(){
         System.out.println("Car1");
+        /*ArrayList*/
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list.contains(2);
+        list.addAll(list2);
+        list.remove(2);
+        list.size();
+        list.add(2);
+        list.clear();
+
+        /*LinkedList*/
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.size();
+        linkedList.add(2);
+        linkedList.addFirst(2);
+        linkedList.addLast(2);
+        linkedList.getLast();
+        linkedList.getFirst();
+        linkedList.contains(2);
+        linkedList.clear();
+
+        /*Stack*/
+        Stack<Integer> stack = new Stack<>();
+        stack.add(2);
+        stack.push(2);
+        stack.empty();
+        stack.peek();
+        stack.pop();
+        stack.clear();
+        stack.contains(2);
+
+        /*HashMap*/
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        hashMap.put(2, 3);
+        hashMap.containsKey(2);
+        hashMap.get(2);
+        hashMap.clear();
+        hashMap.isEmpty();
+        hashMap.size();
+        Set<Map.Entry<Integer, Integer>> entries = hashMap.entrySet();
     }
 }
 
