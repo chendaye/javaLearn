291f7bb5b32247b735e621b6e5d2ed5ccb7a22cc
chendaye666 <chendaye666@gmail.com>
2020-09-21 23:22:14 +0800

    save

---
+++ src/Algorithm/Interview/Company/ZJ/Q5/Main.java
@@ -0,0 +1,17 @@
+package Algorithm.Interview.Company.ZJ.Q5;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("src/Algorithm/Interview/Company/ZJ/Q5/test.txt"));
+
+        Scanner sc = new Scanner(System.in);
+       int N = sc.nextInt();
+
+
+    }
+}
