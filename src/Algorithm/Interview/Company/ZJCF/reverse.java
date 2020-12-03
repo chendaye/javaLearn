73fdffc10d021c5bd61772a5ad1870557840c7fe
chendaye666 <chendaye666@gmail.com>
2020-12-03 23:17:24 +0800

    reverse

---
+++ src/Algorithm/Interview/Company/ZJCF/reverse.java
@@ -0,0 +1,23 @@
+package Algorithm.Interview.Company.ZJCF;
+
+import java.util.Collections;
+
+public class reverse {
+    public static void main(String[] args) {
+        System.out.println(solve("1234"));
+    }
+
+    public static String solve(String str){
+        if (str.length() == 0) return "";
+        char[] chars = str.toCharArray();
+        int l = 0, r = str.length() - 1;
+        while (l <= r){
+            char tmp = chars[l];
+            chars[l++] = chars[r];
+            chars[r--] = tmp;
+        }
+        StringBuilder builder = new StringBuilder();
+        for (char c : chars) builder.append(c);
+        return builder.toString();
+    }
+}
